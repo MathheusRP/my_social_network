53287722cd806111fb686fbe021b1d901fa703aa
Matheus R Pereira <106708093+MathheusRP@users.noreply.github.com>
2023-07-29 12:27:17 -0300

    feat: add friends list in chat

--- src/pages/messages/index.tsx
+++ src/pages/messages/index.tsx
@@ -1,16 +1,55 @@
 import { MessagesStyled } from "./style";
 import { Header } from "../../components/header";
+import { friendList, IfriendList } from "../../data/friendList";
+
 import { IoIosArrowBack } from "react-icons/io"
 import { BsThreeDotsVertical } from "react-icons/bs"
+import { ImSearch } from "react-icons/im"
+import { BsImage, BsFillSendFill } from "react-icons/bs"
 
 export const Messages = () => {
 
     return (
         <MessagesStyled>
-            <Header />
+            <div className="header"><Header /></div>
             <section className="messagesBody">
                 <section className="friendsList">
-
+                    <div className="search">
+                        <input type="text" placeholder="Procurar" />
+                        <ImSearch className="icon" />
+                    </div>
+                    <div className="list">
+                        <ul>
+                            {/* <li>
+                                <div className="friendImage">
+                                    <img src="https://i.pinimg.com/236x/ed/dc/8a/eddc8a518978cd3b749d69dbc5f535e1.jpg" alt="foto de perfil" />
+                                    <span className="status online"></span>
+                                </div>
+                                <div className="friendName">
+                                    <p className="name">Maria Eduarda</p>
+                                    <span>Online</span>
+                                </div>
+                                <BsThreeDotsVertical className="icon" />
+                            </li> */}
+                            {
+                                friendList.map((frind: IfriendList, index: number) => {
+                                    return (
+                                        <li key={index}>
+                                            <div className="friendImage">
+                                                <img src={frind.image} alt={`foto de ${frind.name}`} />
+                                                {frind.online ? (<span className="status online"></span>) : <span className="status offline"></span>}
+                                            </div>
+                                            <div className="friendName">
+                                                <p className="name">{frind.name}</p>
+                                                {frind.online ? (<span>Online</span>) : (<span>Offline</span>)}
+                                            </div>
+                                            <BsThreeDotsVertical className="icon" />
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
                 </section>
                 <section className="messageContainer">
                     <div className="messageHeader">
@@ -29,10 +68,6 @@ export const Messages = () => {
                             <li className="owner message">
                                 <p>Olá, bom dia</p>
                                 <span>10:18</span>
-                            </li>
-                            <li className="message">
-                                <p>Oi</p>
-                                <span>10:20</span>
                             </li>
                             <li className="message">
                                 <p>Amanhã falo com você</p>
@@ -65,16 +100,20 @@ export const Messages = () => {
                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
                                 <span>10:18</span>
                             </li>
-                            <li className="owner message">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                                <span>10:18</span>
-                            </li>
-
                         </ul>
+                    </section>
+                    <section className="toSend">
+                        <div className="inputSection">
+                            <input type="text" placeholder="Mensagem..." />
+                            <div className="icons">
+                                <BsImage className="icon" />
+                                <BsFillSendFill className="icon" />
+                            </div>
+                        </div>
+
                     </section>
                 </section>
                 <section className="messageOwner">
-
                 </section>
             </section>
 
--- src/pages/messages/style.ts
+++ src/pages/messages/style.ts
@@ -3,26 +3,36 @@ import styled from "styled-components";
 export const MessagesStyled = styled.section`
     background-color: var(--grey_2);
     height: 100vh;
+    display: flex;
+    overflow-y: hidden;
+
+    
+    .header {
+        display: none;
+    }
 
     .messagesBody {
         display: flex;
-        
         width: 100%;
+        height: 100%;
 
         .friendsList {
             display: none;
         }
 
+        .messageOwner {
+            display: none; 
+        }
+
+
         .messageContainer {
             display: flex;
             flex-direction: column;
-            /* border: 2px solid red; */
             width: 100%;
-            height: 100vh;
             margin: 0px auto;
-            padding: 80px 0px;
-            max-width: 700px;
+            max-width: 800px;
             box-shadow: 0px 0px 15px rgb(0, 0, 0, 0.3);
+             
 
             .messageHeader {
                 display: flex;
@@ -66,25 +76,27 @@ export const MessagesStyled = styled.section`
             .messagesList {
                 height: 100%;
                 padding: 20px 15px;
-                /* border: 2px solid red; */
                 display: flex;
-                overflow-y: scroll;
+                overflow-y: scroll;    
                 
+                &::-webkit-scrollbar{
+                    background-color: transparent;
+                    width: 5px;
+                }
+
+                &::-webkit-scrollbar-thumb {
+                    background-color: var(--color1);
+                    border-radius: 5px;
+                }
 
                 ul {
                     display: flex;
                     flex-direction: column;
                     width: 100%;
-                    /* height: 100%; */
-                    /* border: 2px solid blue; */
-                    /* justify-content: flex-end; */
                     gap: 15px;
-                    
-                    
                     
                     .date {
                         width: 100%;
-                        /* background-color: var(--grey_1); */
                         height: 40px;
                         display: flex;
                         align-items: center;
@@ -97,21 +109,21 @@ export const MessagesStyled = styled.section`
                     .message {
                         display: flex;
                         flex-direction: column;
-                        /* border: 2px black solid; */
                         max-width: 80%;
                         align-self: flex-start;
                         gap: 5px;
+                        
+                        span {
+                            padding-bottom: 10px;
+                        }
 
                         p {
                             background-color: var(--grey_1);
                             padding: 10px;
                             color: var(--grey_2);
-                            border-radius: 8px 8px 8px 0px;
-                            
+                            border-radius: 8px 8px 8px 0px; 
                         }
                     }
-
-
 
                     .owner {
                         align-self: flex-end;
@@ -126,14 +138,229 @@ export const MessagesStyled = styled.section`
                         }
                     }
                 }
+            }
 
+            .toSend {
+                border-top: 4px solid var(--color1);
+                /* border-radius: 16px; */
+
+                .inputSection {
+                    height: 100%;
+                    width: 100%;
+                    background-color: var(--grey_3);
+                    padding: 0px 15px;
+                    display: flex;
+                    align-items: center;
+                    /* gap: 20px; */
+                    /* border-radius: 16px; */
+                }
+
+                input {
+                    height: 60px;
+                    display: flex;
+                    font-size: 20px;
+                    width: 100%;
+                    align-items: center;
+                    background-color: transparent;
+                    border: none;
+                    outline: none;
+                    padding: 0px 15px;
+                    /* border-radius: 16px; */
+                }
+
+                .icons {
+                    height: 45px;
+                    background-color: var(--color1);
+                    width: fit-content;
+                    display: flex;
+                    gap: 20px;
+                    padding: 0px 20px;
+                    align-items: center;
+                    border-radius: 8px;
+                }
+
+                .icon {
+                    
+                    font-size: 26px;
+                    color: var(--grey_2);
+                }
+            }
+        }
+    }
+
+    @media (min-width: 800px) {
+        .header {
+            display: flex;
+        }
+
+        .messagesBody {
+            margin-top: 120px;
+            height: 82%;
+            /* background-color: aquamarine; */
+            gap: 10px;
+            padding: 0px 15px;
+
+            .messageContainer {
+                width: 60%;
+                max-width: 700px;
+                border-radius: 16px;
+                background-color: var(--grey_3);
+                /* margin-bottom: 20px; */
+
+                .toSend {
+                    border: 4px solid var(--color1);
+                    border-radius: 0px 0px 16px 16px;
+                    
+
+                    .inputSection {
+                        border-radius: 0px 0px 16px 16px;
+                        padding: 0px;
+                    }
+
+                    .icons {
+                        height: 60px;
+                        border-radius: 0px 0px 10px 0px;
+
+                    }
+                }
+            }
+
+            .friendsList {
+                /* position: absolute; */
+                background-color: var(--grey_3);
+                display: flex;
+                flex-direction: column;
+                gap: 15px;
+                width: 35%;
+                max-width: 400px;
+                height: 100%;
+                border-radius: 16px;
+                padding: 25px 15px;
                 
+                .search {
+                    width: 100%;
+                    display: flex;
+                    height: 40px;
+                    color: var(--grey_1);
+                    border: 2px solid var(--color1);
+                    border-radius: 8px;
+                    padding: 0px 15px;
+                    align-items: center;
+                    
+
+                    input {
+                        font-size: 1rem;
+                        font-weight: 600;
+                        background-color: transparent;
+                        border: none;
+                        width: 100%;
+                        outline: none;
+                        padding-right: 15pxs;
+                    }
+
+                    .icon {
+                        font-size: 24px;
+                    }
+                }
+
+                .list {
+                    /* background-color: aquamarine; */
+                    width: 100%;
+                    height: 100%;
+                    overflow-y: auto;
+
+                    &::-webkit-scrollbar{
+                        background-color: transparent;
+                        width: 5px;
+                    }
+
+                    &::-webkit-scrollbar-thumb {
+                        background-color: var(--color1);
+                        border-radius: 5px;
+                    }
+
+                    ul {
+                        width: 100%;
+                        /* background-color: aqua; */
+                        /* border: 2px solid red; */
+                        display: flex;
+                        flex-direction: column;
+                        gap: 10px;
+
+                        li {
+                            display: flex;
+                            height: 80px;
+                            align-items: center;
+                            gap: 15px;
+
+                            
+
+                            .friendImage {
+                                position: relative;
+
+                                img {
+                                    height: 60px;
+                                    width: 60px;
+                                    border-radius: 50%;
+                                    object-fit: cover;
+                                }
+
+                                span {
+                                    height: 16px;
+                                    width: 16px;
+                                    /* background-color: var(--red); */
+                                    position: absolute;
+                                    right: 0px;
+                                    bottom: 6px;
+                                    border-radius: 50%;
+                                }
+
+                                span.online {
+                                    background-color: var(--green);
+                                }
+
+                                span.offline {
+                                    background-color: var(--red);
+                                }
+                            }
+
+                            .friendName {
+                                display: flex;
+                                flex-direction: column;
+                                width: 100%;
+
+                                p {
+                                    font-size: 1.2rem;
+                                    font-weight: bold;
+                                    color: var(--grey_0);
+                                    
+                                }
+                            }
+
+                            .icon {
+                                justify-self: flex-end;
+                                font-size: 20px;
+                                width: 30px;
+                                opacity: 0%;
+                                /* background-color: aqua; */
+                            }
+
+                            /* .friendName:hover ~ .icon{
+                                background-color: aqua;
+                            } */
+                           
+                            &:hover {
+                                .icon {
+                                    opacity: 100%;
+                                }
+                            }
+                        }
+
+                    }
+                }
             }
         }
 
-        .messageOwner {
-            display: none;
-           
-        }
+        
     }
 `
