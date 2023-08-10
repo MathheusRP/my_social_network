05a4a515500659c566cb4b461034e397bb5156c7
Matheus R Pereira <106708093+MathheusRP@users.noreply.github.com>
2023-08-10 18:18:44 -0300

    refactor: update photo page (#10)

--- src/components/photo/index.tsx
+++ src/components/photo/index.tsx
@@ -1,8 +1,9 @@
-import { PhotoStyle } from "./style"
-import { PostButtons } from "../postButtons";
+import { PostOpenStyled } from "./styled";
 
 import { AiFillLike, AiOutlineSend, AiOutlineCloseCircle } from "react-icons/ai"
 import { BsFillEmojiLaughingFill } from "react-icons/bs"
+
+import { AiOutlineLike, AiOutlineComment, AiOutlineShareAlt, AiFillCaretDown } from "react-icons/ai"
 
 import { useNavigate } from "react-router-dom";
 
@@ -20,139 +21,147 @@ export const Photo = () => {
         navigate(-1)
     }
 
-    const [noImage, setNoImage] = useState<string>("haveImage")
+    // const [noImage, setNoImage] = useState<string>("haveImage")
 
-    useEffect(() => {
-        if (postOpen?.img) {
-            setNoImage("haveImage")
-        } else {
-            setNoImage("noImage")
+    // useEffect(() => {
+    //     if (postOpen?.img) {
+    //         setNoImage("haveImage")
+    //     } else {
+    //         setNoImage("noImage")
+    //     }
+    // }, [postOpen?.img])
+
+    const [infoStatus, setInfoStatus] = useState<string>("visible")
+    const [infoDisplay, setInfoDisplay] = useState<string>("flex")
+
+    const hidenInfo = (e: any) => {
+        if (e.target.localName === "div") {
+
+            if (infoStatus === "visible") {
+                setInfoStatus("hidden")
+                setTimeout(() => {
+                    setInfoDisplay("none")
+                }, 900);
+            } else {
+                setInfoStatus("visible")
+                setInfoDisplay("flex")
+            }
         }
-    }, [postOpen?.img])
+
+    }
+
+    const [commentStatus, setCommentStatus] = useState<string>("close")
+    const [commentDisplay, setCommentDisplay] = useState<string>("none")
+
+    const commentStatusFunction = () => {
+        if (commentDisplay === "open") {
+            setCommentStatus("close")
+            setTimeout(() => {
+                setCommentDisplay("none")
+            }, 900);
+        } else {
+            setCommentStatus("open")
+            setCommentDisplay("open")
+        }
+    }
 
     return (
-        <PhotoStyle>
+        <PostOpenStyled>
             {postOpen ? (
                 <>
-                    <div className={`imgContainer ${noImage}`}>
-                        {
-                            postOpen.img ? (<img src={postOpen.img} alt="foto de perfil" />) : (<></>)
-                        }
-                        <AiOutlineCloseCircle className="icon" onClick={() => closeImage()} />
-                    </div>
-                    <section className={`postDetail ${noImage}`}>
-                        <AiOutlineCloseCircle className="close" onClick={() => closeImage()} />
-                        <div className="ownerContainer">
-                            <img src={postOpen.owner.image} alt={`foto de ${postOpen.owner.name}`} />
-                            <div className="info">
-                                <p>{postOpen.owner.name}</p>
-                                <span>{postOpen.date}</span>
-                            </div>
+                    <div className="postContent">
+                        <div id="img" onClick={(e) => hidenInfo(e)} className="imageContainer">
+                            {/* <img src="https://fusne.com/wp-content/uploads/2023/06/lofi-girl-fusne.jpg" alt="teste" /> */}
+                            {postOpen.img ? (<img className="postImage" src={postOpen.img} alt="teste" />) : (<></>)}
+
                         </div>
-                        <div className="text">
-                            <p>{postOpen.description}</p>
-                        </div>
-                        <div>
-                            <PostButtons comments={Number(postOpen.comments?.length)} id="1" likes={postOpen.likes} key={2} />
+                        <AiOutlineCloseCircle className={`icon ${commentStatus} ${infoStatus}`} onClick={() => closeImage()} />
+                        <div onClick={(e) => hidenInfo(e)} className="detailAndComments">
+                            <section id="detail" className={`postDetail ${infoDisplay} ${infoStatus}`}>
+                                <section className="info">
+                                    <div className="owner">
+                                        <img src="https://lh3.googleusercontent.com/-kSnNyDkgd2E/AAAAAAAAAAI/AAAAAAAABcM/DimvpU0bUPY/photo.jpg" alt="foto de perfil" />
+                                        <div>
+                                            <h3>{postOpen.owner.name}</h3>
+                                            <p>{postOpen.date}</p>
+                                        </div>
+                                    </div>
+                                    <div className="text">
+                                        <p>{postOpen.description}</p>
+
+                                    </div>
+                                </section>
+                                <section className="likesEndComment">
+                                    <div className="likesAndCommentsInfo">
+                                        <p>{postOpen.likes} likes</p>
+                                        <p onClick={() => commentStatusFunction()}>{postOpen.comments?.length} Comentarios</p>
+                                    </div>
+                                    <div className="button">
+                                        <div>
+                                            <AiOutlineLike className="icons" />
+                                            <p>Curti</p>
+                                        </div>
+                                        <div>
+                                            <AiOutlineComment className="icons" />
+                                            <p onClick={() => commentStatusFunction()}>Comentar</p>
+                                        </div><div>
+                                            <AiOutlineShareAlt className="icons" />
+                                            <p>Compartilhar</p>
+                                        </div>
+                                    </div>
+                                </section>
+                            </section>
+                            <section id="comment" className={`comment ${commentStatus} ${commentDisplay}`}>
+                                <button onClick={() => commentStatusFunction()} type="button"><AiFillCaretDown /></button>
+                                <div className="filter">
+                                    <select name="" id="">
+                                        <option value="value-1">Mais relevante</option>
+                                        <option value="value-2">Mais recentes</option>
+                                        <option value="value-3">Todos</option>
+                                    </select>
+                                </div>
+                                <ul>
+                                    {
+                                        postOpen.comments?.map((comment, index) => {
+                                            return (
+                                                <li key={index}>
+                                                    <img src={comment.image} alt={`Foto de ${comment.name}`} />
+                                                    <div className="commentContainer">
+                                                        <h4 className="name">{comment.name}</h4>
+                                                        <p >{comment.comment}</p>
+                                                    </div>
+                                                    <div className="likes">
+                                                        <AiFillLike />
+                                                        <p>{comment.likes}</p>
+                                                    </div>
+                                                    <div className="buttons">
+                                                        <p>Like</p>
+                                                        <p>Comentar</p>
+                                                    </div>
+                                                </li>
+                                            )
+                                        })
+                                    }
+
+
+                                    <span>Mais comentarios</span>
+                                </ul>
+                                <div className="input">
+                                    <input type="text" placeholder="Comentar..." />
+                                    <div>
+                                        <BsFillEmojiLaughingFill className="inputIcon" />
+                                        <AiOutlineSend className="inputIcon" />
+                                    </div>
+                                </div>
+                            </section>
                         </div>
 
-                        <ul>
-                            {
-                                postOpen.comments?.map((comment, index) => {
-                                    return (
-                                        <li key={index}>
-                                            <img src={comment.image} alt={`foto de ${comment.name}`} />
-                                            <div className="commentContainer">
-                                                <p className="name">{comment.name}</p>
-                                                <p className="comment">{comment.comment}</p>
-                                            </div>
-                                            <div className="likes">
-                                                <AiFillLike />
-                                                <p>{comment.likes}</p>
-                                            </div>
-                                            <div className="buttons">
-                                                <p>Like</p>
-                                                <p>Comentar</p>
-                                            </div>
-                                        </li>
-                                    )
-                                })
-                            }
-                            {/* <li>
-                                <img src="https://lh3.googleusercontent.com/-kSnNyDkgd2E/AAAAAAAAAAI/AAAAAAAABcM/DimvpU0bUPY/photo.jpg" alt="foto de perfil" />
-                                <div className="commentContainer">
-                                    <p className="name">Larissa Silva</p>
-                                    <p className="comment">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                                </div>
-                                <div className="likes">
-                                    <AiFillLike />
-                                    <p>6</p>
-                                </div>
-                                <div className="buttons">
-                                    <p>Like</p>
-                                    <p>Comentar</p>
-                                </div>
-                            </li> */}
-                            {/* <li>
-                                <img src="https://lh3.googleusercontent.com/-kSnNyDkgd2E/AAAAAAAAAAI/AAAAAAAABcM/DimvpU0bUPY/photo.jpg" alt="foto de perfil" />
-                                <div className="commentContainer">
-                                    <p className="name">Larissa Silva</p>
-                                    <p className="comment">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                                </div>
-                                <div className="likes">
-                                    <AiFillLike />
-                                    <p>6</p>
-                                </div>
-                                <div className="buttons">
-                                    <p>Like</p>
-                                    <p>Comentar</p>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="https://lh3.googleusercontent.com/-kSnNyDkgd2E/AAAAAAAAAAI/AAAAAAAABcM/DimvpU0bUPY/photo.jpg" alt="foto de perfil" />
-                                <div className="commentContainer">
-                                    <p className="name">Larissa Silva</p>
-                                    <p className="comment">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                                </div>
-                                <div className="likes">
-                                    <AiFillLike />
-                                    <p>6</p>
-                                </div>
-                                <div className="buttons">
-                                    <p>Like</p>
-                                    <p>Comentar</p>
-                                </div>
-                            </li>
-                            <li>
-                                <img src="https://lh3.googleusercontent.com/-kSnNyDkgd2E/AAAAAAAAAAI/AAAAAAAABcM/DimvpU0bUPY/photo.jpg" alt="foto de perfil" />
-                                <div className="commentContainer">
-                                    <p className="name">Larissa Silva</p>
-                                    <p className="comment">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                                </div>
-                                <div className="likes">
-                                    <AiFillLike />
-                                    <p>6</p>
-                                </div>
-                                <div className="buttons">
-                                    <p>Like</p>
-                                    <p>Comentar</p>
-                                </div>
-                            </li> */}
-                        </ul>
-                        <section className="newComment">
-                            <div>
-                                <input type="text" placeholder="Comentar..." />
-                                <BsFillEmojiLaughingFill className="icon" />
-                                <AiOutlineSend className="icon" />
-                            </div>
-                        </section>
-                    </section>
+                    </div>
+
                 </>
             ) : (
                 <></>
             )}
-        </PhotoStyle>
+        </PostOpenStyled>
     )
 }
-
-// https://fusne.com/wp-content/uploads/2023/06/lofi-girl-fusne.jpg
